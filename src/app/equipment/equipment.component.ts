b3f736c4fe23dbc1760fe67fc3aaac2cb0539326
Vit-Knysh <v.knysh85@gmail.com>
2022-07-01 00:13:46 -0400

    Loop for equipment

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
-
+  equipmentItems: string [] = ['Command Module', 'Space Camera', 'Food', 'Oxygen Tanks'];
+  
   constructor() { }
 
   ngOnInit() {
